ddbe0a7d394bd6a4b35b20a8e17a1375873e8584
ecarlosqm <krlosz.qm@gmail.com>
2021-05-10 01:31:54 -0500

    Adds Google Maps Plugin

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
 import UIKit
 import Flutter
+import Foundation
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +9,9 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Google Maps
+    GMSServices.provideAPIKey("YOUR KEY HERE")
+    // Google Maps
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
